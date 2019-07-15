ded1058da00845f23a97130f3f67c5c5408894a3
Exaltrse <52892387+Exaltrse@users.noreply.github.com>
2019-07-15 18:19:43 +0300

    Обновление Мейна с проверками заданий до 13-го включительно.

--- Main_DZ2.java
+++ Main_DZ2.java
@@ -1,9 +1,7 @@
 import java.util.Random;
 
-public class Main_DZ2
-{
-    public static void main(String[] args)
-    {
+public class Main_DZ2 {
+    public static void main(String[] args) {
         Zadanija z = new Zadanija();
 
         {
@@ -29,7 +27,7 @@ public class Main_DZ2
             int[] tmpArray = z.CreateRandomIntArray(16);
             System.out.println("Исходный массив: ");
             z.PrintArray(tmpArray);
-            System.out.println("\nСумма всех элементов массива: "+z.Task2_Sum(tmpArray));
+            System.out.println("\nСумма всех элементов массива: " + z.Task2_Sum(tmpArray));
             System.out.println("\n__________");
         }
 
@@ -38,7 +36,7 @@ public class Main_DZ2
             int[] tmpArray = z.CreateRandomIntArray(16);
             System.out.println("Исходный массив: ");
             z.PrintArray(tmpArray);
-            System.out.println("\nСреднее значение элементов массива: "+z.Task4_CalculateAvg(tmpArray));
+            System.out.println("\nСреднее значение элементов массива: " + z.Task4_CalculateAvg(tmpArray));
             System.out.println("\n__________");
         }
 
@@ -47,9 +45,9 @@ public class Main_DZ2
             int[] tmpArray = z.CreateRandomIntArray(16);
             System.out.println("Исходный массив: ");
             z.PrintArray(tmpArray);
-            String message1 = z.Task5_Contains(tmpArray, tmpArray[6])?" содержит":" не содержит";
-            String message2 = z.Task5_Contains(tmpArray, 35)?" содержит":" не содержит";
-            System.out.println("\nМассив"+message1+" число "+tmpArray[6] +" и " + message2 + " число 35.");
+            String message1 = z.Task5_Contains(tmpArray, tmpArray[6]) ? " содержит" : " не содержит";
+            String message2 = z.Task5_Contains(tmpArray, 35) ? " содержит" : " не содержит";
+            System.out.println("\nМассив" + message1 + " число " + tmpArray[6] + " и " + message2 + " число 35.");
             System.out.println("\n__________");
         }
 
@@ -59,14 +57,106 @@ public class Main_DZ2
             System.out.println("Исходный массив: ");
             z.PrintArray(tmpArray);
             int index = z.Task6_FirstIndexOf(tmpArray, tmpArray[6]);
-            System.out.println("\nИндекс элемента "+tmpArray[6]+" - "+index +" .");
+            System.out.println("\nИндекс элемента " + tmpArray[6] + " - " + index + " .");
             System.out.println("\n__________");
         }
 
-        //System.out.println("Задание 2.\nРезультаты суммирования элементов массива: "+sum+"\n_____");
+        {
+            System.out.println("Задание 7.");
+            int[] tmpArray = z.CreateRandomIntArray(16);
+            System.out.println("Исходный массив: ");
+            z.PrintArray(tmpArray);
+            int index = 6;
+            tmpArray = z.Task7_Remove(tmpArray, index);
+            System.out.println("\nМассив после преобразований (удален индекс " + index + "): ");
+            z.PrintArray(tmpArray);
+            System.out.println("\n__________");
+        }
+
+        {
+            System.out.println("Задание 8.");
+            int[] tmpArray = z.CreateRandomIntArray(16);
+            System.out.println("Исходный массив: ");
+            z.PrintArray(tmpArray);
+            int[] resultArray = z.Task8_ArrayIteratingCopy(tmpArray);
+            System.out.println("\nМассив 2: ");
+            z.PrintArray(resultArray);
+            System.out.println("\n__________");
+        }
+
+        {
+            System.out.println("Задание 9.");
+            int[] tmpArray = z.CreateRandomIntArray(16);
+            System.out.println("Исходный массив: ");
+            z.PrintArray(tmpArray);
+            int index = 6, value = 152;
+            tmpArray = z.Task9_InsertValueToIndex(tmpArray, index, value);
+            System.out.println("\nМассив после преобразований (в индекс " + index + " добавлено значение " + value + "): ");
+            z.PrintArray(tmpArray);
+            System.out.println("\n__________");
+        }
+
+        {
+            System.out.println("Задание 10.");
+            int[] tmpArray = z.CreateRandomIntArray(16);
+            System.out.println("Исходный массив: ");
+            z.PrintArray(tmpArray);
+            System.out.println("");
+            z.Task10_MaxAndMinValue(tmpArray);
+            System.out.println("\n__________");
+        }
+
+        {
+            System.out.println("Задание 11.");
+            int[] tmpArray = z.CreateRandomIntArray(16);
+            System.out.println("Исходный массив: ");
+            z.PrintArray(tmpArray);
+            tmpArray =z.Task11_Reverse(tmpArray);
+            System.out.println("\nМассив после преобразований: ");
+            z.PrintArray(tmpArray);
+            System.out.println("\n__________");
+        }
+
+        {
+            System.out.println("Задание 12.");
+            int[] tmpArray = z.CreateRandomIntArray(16);
+            Random r = new Random();
+            for(int i=0; i<10; i++)
+            {
+                tmpArray = z.Task9_InsertValueToIndex(tmpArray, tmpArray.length, tmpArray[r.nextInt(15)]);
+            }
+            System.out.println("Исходный массив: ");
+            z.PrintArray(tmpArray);
+            int[] result = z.Task12_FindDuplicates(tmpArray);
+            if(result.length<1)
+            {
+                System.out.println("В массиве не найдено дубликатов.");
+            }
+            else
+            {
+                System.out.println("\nНайденные в массиве дубликаты: ");
+                z.PrintArray(result);
+            }
+            System.out.println("\n__________");
+        }
+
+        {
+            System.out.println("Задание 13.");
+            String[] tmpArray = z.CreateStringArray();
+            Random r = new Random();
+            for(int i=0; i<6;i++)
+            {
+                tmpArray = z.AddIndexToArray(tmpArray, tmpArray.length, tmpArray[r.nextInt(6)]);
+            }
+            System.out.println("Исходный массив: ");
+            z.PrintSArray(tmpArray);
+            String[] results = z.Task13_DuplicatesStrings(tmpArray);
+            System.out.println("\nНайденные дубликаты строк: ");
+            z.PrintSArray(results);
+            System.out.println("\n__________");
+        }
+
     }
-
-
 
 
 }
